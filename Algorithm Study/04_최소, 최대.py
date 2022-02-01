f16c8bbdb7f2f135ec704f7fdd2050793264dadc
zziyyung <jiyoung99919@gmail.com>
2022-02-01 16:14:03 +0900

    algorithm

--- Algorithm Study/04_최소, 최대.py
+++ Algorithm Study/04_최소, 최대.py
@@ -1,9 +1,48 @@
-T=int(input())
-Input=input()
+#백준 10818번
+import sys
+N=int(sys.stdin.readline())
+Input=[int(x) for x in sys.stdin.readline().split() if -1000000<=int(x)<=1000000]
+#[map(int,sys.stdin.readline().split())] -- >개수 왜 안되지 ?
+#리스트 컴프리헨션은 반복문 ! !!!
+#list(map(int,sys.stdin.readline().split())) ->됨
 
-a=list(map(int,Input.split()))
-print(min(a),max(a))
 
+if (N != len(Input)) or (N<1) or (N>1000000):
+    print("다시 입력")
+else:
+    minIdx=0
+    maxIdx=0
+    for i in range(len(Input)):
+        if Input[i]<Input[minIdx]:
+            minIdx=i
+        elif Input[i]>Input[maxIdx]:
+            maxIdx=i
+    print(f'{Input[minIdx]} {Input[maxIdx]}')
+
+
+#함수 선언부
+선택정렬 함수
+def selectionSort(ary):
+    n=len(ary)
+    for i in range(0,n-1):
+        minIdx=i
+        for j in range(i+1,n):
+            if (ary[minIdx]>ary[j]):
+                minIdx=j
+        ary[i],ary[minIdx]=ary[minIdx],ary[i]
+    return ary
+
+##전역 변수부
+import sys
+N=int(sys.stdin.readline())
+Input=[int(x) for x in sys.stdin.readline().split()]
+
+##메인 코드
+selectionSort(Input)
+print(f'{Input[0]} {Input[-1]}')
+
+
+#쩡님
 
 # N개의 정수가 주어질 때, 최솟값과 최댓값을 구하는 프로그램을 작성하시오.
 ## 1) 선택정렬....시간초과......ㅜㅜ
@@ -25,21 +64,21 @@ print(min(a),max(a))
 
 
 ## 2) 성공!
-try:
-    n = int(input())
-    a = [i for i in map(int, input().split())]
-    if n != len(a):
-        print(f'정수 {n}개를 적어주세요!')
-    min_ = 0
-    max_ = 0
-    for i in range(len(a)):
-        if a[i] < a[min_]:
-            min_ = i
-        if a[i] > a[max_]:
-            max_ = i
-    print(a[min_], a[max_])
-except Exception as e: # 20 10 35 30 7
-    print(e)
+# try:
+#     n = int(input())
+#     a = [i for i in map(int, input().split())]
+#     if n != len(a):
+#         print(f'정수 {n}개를 적어주세요!')
+#     min_ = 0
+#     max_ = 0
+#     for i in range(len(a)):
+#         if a[i] < a[min_]:
+#             min_ = i
+#         if a[i] > a[max_]:
+#             max_ = i
+#     print(a[min_], a[max_])
+# except Exception as e: # 20 10 35 30 7
+#     print(e)
 
 # i    min
 # a[0]=20 a[0]=20 i=0 min_=0
@@ -59,18 +98,18 @@ except Exception as e: # 20 10 35 30 7
 #             break
 # print(a[0],a[-1])
 
-
+#혠님
 
 # 첫째 줄에 입력한 정수의 갯수
-while True :
-    N = int(input())
-    if 1 <= N <= 1000000:
-        break
-
-# 둘째 줄에 입력한 N개의 정수
-num = input()
-new_list=list(map(int,num.split()))
-
-# 리스트 정렬 후 최솟값과 최댓값 출력
-new_list.sort()
-print(f'{new_list[0]} {new_list[len(new_list)-1]}')
+# while True :
+#     N = int(input())
+#     if 1 <= N <= 1000000:
+#         break
+#
+# # 둘째 줄에 입력한 N개의 정수
+# num = input()
+# new_list=list(map(int,num.split()))
+#
+# # 리스트 정렬 후 최솟값과 최댓값 출력
+# new_list.sort()
+# print(f'{new_list[0]} {new_list[len(new_list)-1]}')
