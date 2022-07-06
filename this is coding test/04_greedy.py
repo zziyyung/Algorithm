43af0e13332948c67d811f60799497525d210ddc
zziyyung <jiyoung99919@gmail.com>
2022-07-06 12:00:40 +0900

    220706_greedy/algorithm_update

--- this is coding test/04_greedy.py
+++ this is coding test/04_greedy.py
@@ -17,3 +17,25 @@ while N != 1 :
         cnt += 1
 
 print(cnt)
+
+
+N,K = map(int,input().split())
+
+cnt = 0
+
+while True:
+    # (N == K로 나누어 떨어지는 수)가 될 때까지 1씩 빼기
+    target = (N//K) * K
+    cnt += (N - target)
+    N = target
+
+    # N이 K보다 작을 때 (더 이상 나눌 수 없을 때) 반복문 탈출
+    if N < K:
+        break
+
+    cnt += 1
+    N //= K
+
+# 마지막으로 남은 수에 대하여 1씩 빼기
+cnt += (N-1)
+print(cnt)
--- this is coding test/09_greedy.py
+++ this is coding test/09_greedy.py
@@ -1,4 +1,50 @@
 # 볼링공 고르기
 
-# 서로 무게가 다른공
-# 문제 예시를 먼저 이해해보자
+import time
+
+start_time = time.time()  # 측정 시작
+
+##### 나의 풀이
+
+N,M = map(int,input().split())
+data = list(map(int,input().split()))
+
+sum = 0
+count = N-1
+
+# 등차수열 (합 구하기
+for _ in range(len(data)):
+    sum += count
+    count = count - 1
+
+#print(sum)
+
+# 리스트 내 중복값 카운트하기
+# 구글링 set 자료형 !
+
+dup = len(data) - len(set(data))
+
+print(sum-dup)
+
+##### 책 풀이
+
+array = [0] * 11  # 배열 내 11개 요소 생김 0~11 ( 0번째는 버리고 1~10 사용)
+# print(array)
+
+for x in data:
+    array[x] += 1
+
+#print(array)
+
+result = 0
+
+for i in range(1,N+1):   # 1부터 n까지 각 무게에 대하여 처리
+    N -= array[i]  # B가 선택할 수 있는 경우의 수
+    result += array[i] * N
+
+print(result)
+
+
+# 프로그램 소스코드
+end_time = time.time()  # 측정 종료
+print('time :', end_time - start_time)  # 수행시간 출력
