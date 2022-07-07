d75d9e3b3e873cf1b99dbee43fae4d5eeaaf21ac
zziyyung <jiyoung99919@gmail.com>
2022-07-07 10:13:26 +0900

    220707_greedy

---
+++ This is a coding test/greedy/11_greedy.py
@@ -0,0 +1,16 @@
+# 2839번 설탕배달
+
+N = int(input())
+
+cnt = 0
+
+while N >= 0:
+    if N % 5 == 0:
+        cnt += (N//5)
+        print(cnt)
+        break
+    N -= 3
+    cnt += 1
+
+else:
+    print(-1)
