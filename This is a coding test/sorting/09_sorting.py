7d0b310aa43b28e778b7de48ee55c99ee55ff5f4
zziyyung <jiyoung99919@gmail.com>
2022-08-20 23:39:05 +0900

    220820_ 두 배열의 원소 교체

--- This is a coding test/sorting/09_sorting.py
+++ This is a coding test/sorting/09_sorting.py
@@ -1,32 +1,33 @@
 # 두 배열의 원소 교체
 
-# # N개의 원소와 최대 K번의 바꿔치기
-# N , K = map(int,input().split())
-#
-# # A,B의 원소들
-# A = list(map(int,input().split()))
-# B = list(map(int,input().split()))
-#
-# # 원소를 바꿔 최댓값 구하기
-# # A의 원소는 오름차순으로 , B의 원소는 내림차순으로
-# Asum = 0
-#
-# sortA = sorted(A)
-# sortB = sorted(B,reverse=True)
-#
-# for i in range(N):
-#     if i < K:
-#         if sortA[i] <= sortB[i]:
-#             Asum += sortB[i]
-#         else:
-#             Asum += sortA[i]
-#     else:
-#         Asum += sortA[i]
-#
-# print(Asum)
+# 1차 풀이
+# N개의 원소와 최대 K번의 바꿔치기
+N , K = map(int,input().split())
+
+# A,B의 원소들
+A = list(map(int,input().split()))
+B = list(map(int,input().split()))
+
+# 원소를 바꿔 최댓값 구하기
+# A의 원소는 오름차순으로 , B의 원소는 내림차순으로
+Asum = 0
+
+sortA = sorted(A)
+sortB = sorted(B,reverse=True)
+
+for i in range(N):
+    if i < K:
+        if sortA[i] <= sortB[i]:
+            Asum += sortB[i]
+        else:
+            Asum += sortA[i]
+    else:
+        Asum += sortA[i]
+
+print(Asum)
 
 
-# 책 답안
+# 2차
 N , K = map(int,input().split())
 
 A = list(map(int,input().split()))
