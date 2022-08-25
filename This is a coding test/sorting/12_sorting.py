1f79f872e1e925e39108b6a98f9618bd1a246b2d
zziyyung <jiyoung99919@gmail.com>
2022-08-25 23:36:35 +0900

    220825_ 실패율

--- This is a coding test/sorting/12_sorting.py
+++ This is a coding test/sorting/12_sorting.py
@@ -1,2 +1,18 @@
 # 실패율
 
+def solution(N, stages):
+    length = len(stages)
+    result = []
+    for i in range(1, N + 1):
+        count = stages.count(i)
+
+        if length == 0:
+            fail = 0
+        else:
+            fail = count / length
+
+        length -= count
+        result.append((i, fail))
+    result = sorted(result, key=lambda x: (-x[1], x[0]))
+    answer = [res[0] for res in result]
+    return answer
