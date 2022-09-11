fad75fba805f895b73cbb99be672eadb6f93c319
zziyyung <jiyoung99919@gmail.com>
2022-09-11 23:40:04 +0900

    220911_ 떡볶이 떡 만들기

---
+++ This is a coding test/binary search/05_binary search.py
@@ -0,0 +1,24 @@
+# 떡볶이 떡 만들기
+
+n, m = list(map(int, input().split()))
+
+arr = list(map(int, input().split()))
+
+start = 0
+end = max(arr)
+
+result = 0
+while (start <= end):
+    total = 0
+    mid = (start + end) // 2
+    for x in arr:
+        if x > mid:
+            total += x - mid
+
+    if total < m:
+        end = mid - 1
+    else:
+        result = mid
+        start = mid + 1
+
+print(result)
