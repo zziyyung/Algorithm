9b8ac386c219311ae3264c334f569324c195b59e
zziyyung <96896919+zziyyung@users.noreply.github.com>
2024-06-06 21:48:59 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -0,0 +1,14 @@
+import itertools
+import sys
+
+heights = list(int(sys.stdin.readline().strip()) for _ in range(9))
+
+for i in range(len(heights)):
+    for j in range(i+1, len(heights)):
+        if sum(heights) - (heights[i] + heights[j]) == 100:
+            x,y = heights[i], heights[j]
+
+heights.remove(x)
+heights.remove(y)
+
+print('\n'.join(map(str,sorted(heights))))
