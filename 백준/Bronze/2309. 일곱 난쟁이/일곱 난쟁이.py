2524b50fa928d87fd2b75f7a9c60cc0cc5a87b3b
zziyyung <96896919+zziyyung@users.noreply.github.com>
2024-06-06 21:51:46 +0900

    Update 일곱 난쟁이.py

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,4 +1,3 @@
-import itertools
 import sys
 
 heights = list(int(sys.stdin.readline().strip()) for _ in range(9))
@@ -7,7 +6,8 @@ for i in range(len(heights)):
     for j in range(i+1, len(heights)):
         if sum(heights) - (heights[i] + heights[j]) == 100:
             x,y = heights[i], heights[j]
-
+        break
+        
 heights.remove(x)
 heights.remove(y)
 
