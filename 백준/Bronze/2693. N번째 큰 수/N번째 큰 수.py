adb7e0116c75bc2b3450cc02917cc00e265183f9
zziyyung <96896919+zziyyung@users.noreply.github.com>
2024-06-06 22:44:05 +0900

    [Bronze I] Title: N번째 큰 수, Time: 100 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2693. N번째 큰 수/N번째 큰 수.py
@@ -0,0 +1,11 @@
+import sys
+
+for _ in range(int(sys.stdin.readline().strip())):
+    nums = list(map(int,input().split()))
+    
+    for i in range(1,len(nums)):
+        for j in range(i,0,-1):
+            if nums[j-1] > nums[j]:
+                nums[j-1], nums[j] = nums[j], nums[j-1]
+
+    print(nums[-3])
