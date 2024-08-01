08fcf7c1c1fe92d48aed9209aeb5571eb57ac51e
zziyyung <96896919+zziyyung@users.noreply.github.com>
2024-08-01 23:13:05 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 73.83 ms, Memory: 24.7 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -7,9 +7,7 @@ def solution(participant, completion):
     # 2. 반복문 돌면서 비교한다
     for i in range(len(completion)):
         if participant[i] != completion[i]:
-            answer = participant[i]
-            break
+            return participant[i]
 
-    if answer == '':
-        answer = participant[-1]
-    return answer
+    # 전부 다 돌아도 없을 경우에 마지막 주자가 미완주자
+    return participant[-1]
